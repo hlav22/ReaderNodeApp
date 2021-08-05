c1c6d62a60c7855c91b2e7a6ba9bbb306dd405b3
hlav22 <hgl0006@mix.wvu.edu>
2021-08-05 10:31:06 -0400

    Added Attributes from Data Provided

--- reader.js
+++ reader.js
@@ -21,7 +21,7 @@
          let csvStream = csv
              .parse()
              .on('data', function (data) {
-                 dataObject = new features(data[0], data[1], data[2], data[3]);
+                 dataObject = new features(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24], data[25], data[26], data[27], data[28], data[29], data[30], data[31], data[32], data[33], data[34], data[35], data[36], data[37]);
                  csvData.push(dataObject);
              })
              .on('end', function () {
